199bc7bd473384f3c3c6d0c450952acf44d189db
ajay198810 <56945687+abschill@users.noreply.github.com>
2021-06-29 11:09:25 -0400

    Updates

---
+++ precache-manifest.84a27ce4693bb3c094c38a2ad10f93d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc42eef33fc8b86fd82e13c47414796",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7af429816cf42d727453",
+    "url": "./static/css/main.9d9d1abe.chunk.css"
+  },
+  {
+    "revision": "1049ae651eeaa3b97db6",
+    "url": "./static/js/2.08a8fccf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.08a8fccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af429816cf42d727453",
+    "url": "./static/js/main.e4b14f07.chunk.js"
+  },
+  {
+    "revision": "28045bcd0a229961a287",
+    "url": "./static/js/runtime-main.749eba3a.js"
+  }
+]);
